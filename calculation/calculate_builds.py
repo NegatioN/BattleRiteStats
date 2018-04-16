c3817e3aa61c426be3051ea9c1c0edd70ee0a235
NegatioN <joakimrishaug@gmail.com>
2018-04-16 21:42:56 +0000

    increase rate-limit

--- calculation/calculate_builds.py
+++ calculation/calculate_builds.py
@@ -19,7 +19,7 @@ origin = mk_origin('https', 'api.dc01.gamelockerapp.com', '/shards/global')
 headers = {'Accept': 'application/vnd.api+json',
            'Accept-Encoding': 'gzip',
            'Authorization': 'Bearer {0}'.format(api_key)}
-rate_limiter = RateLimiter(max_calls=50, period=61)
+rate_limiter = RateLimiter(max_calls=100, period=61)
 
 telem_cache = TelemetryCache()
 
