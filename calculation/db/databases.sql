13d62f96f18daf18da532f8eaab94453fc9a2423
NegatioN <joakimrishaug@gmail.com>
2018-05-13 19:59:22 +0200

    allow pyscopg user to gain access to other created tables in battleritedb

--- calculation/db/databases.sql
+++ calculation/db/databases.sql
@@ -17,3 +17,4 @@ CREATE TABLE IF NOT EXISTS "playerteams" (
 
 alter user psycopg with encrypted password 'pass';
 grant all privileges on database battleritebuilds to psycopg;
+alter default privileges in schema public grant all on tables to psycopg;
