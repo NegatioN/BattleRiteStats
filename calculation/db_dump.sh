108213110fb7961578c215c24c13443eff883f56
NegatioN <joakimrishaug@gmail.com>
2018-05-24 21:51:00 +0200

    copy csv to tmp first, then transfer to assets-folder

--- calculation/db_dump.sh
+++ calculation/db_dump.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 export PROJECT_PATH="/home/joakim/projects/BattleRiteStats/calculation/assets"
+export TMP_PATH="/tmp"
 export DB="battleritebuilds"
 
 export UNIX_TIMESTAMP=$(date +%s)
@@ -11,5 +12,10 @@ export MAIN_DF_NAME="dumped_character_df.csv"
 echo "Dumping data from ${DATA_FROM_TIMESTAMP}"
 
 
-sudo -u postgres psql -d ${DB} -c "COPY (SELECT mr.* FROM matchround mr LEFT JOIN playermatch pm ON mr.matchid = pm.matchid WHERE pm.timee > ${DATA_FROM_TIMESTAMP}) TO '${PROJECT_PATH}/${MATCH_DF_NAME}' WITH (FORMAT CSV, HEADER);"
-sudo -u postgres psql -d ${DB} -c "COPY (SELECT * FROM playermatch WHERE timee > ${DATA_FROM_TIMESTAMP}) TO '${PROJECT_PATH}/${MAIN_DF_NAME}' WITH (FORMAT CSV, HEADER);"
+sudo -u postgres psql -d ${DB} -c "COPY (SELECT mr.* FROM matchround mr LEFT JOIN playermatch pm ON mr.matchid = pm.matchid WHERE pm.timee > ${DATA_FROM_TIMESTAMP}) TO '${TMP_PATH}/${MATCH_DF_NAME}' WITH (FORMAT CSV, HEADER);"
+sudo -u postgres psql -d ${DB} -c "COPY (SELECT * FROM playermatch WHERE timee > ${DATA_FROM_TIMESTAMP}) TO '${TMP_PATH}/${MAIN_DF_NAME}' WITH (FORMAT CSV, HEADER);"
+
+cp ${TMP_PATH}/${MATCH_DF_NAME} ${PROJECT_PATH}/${MATCH_DF_NAME}
+cp ${TMP_PATH}/${MAIN_DF_NAME} ${PROJECT_PATH}/${MAIN_DF_NAME}
+
+sudo rm ${TMP_PATH}/${MAIN_DF_NAME} ${TMP_PATH}/${MATCH_DF_NAME}
